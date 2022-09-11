25521a2e6f69206e4248b32636096541fdae2148
Carlos Morales <dev.carlos.morales@gmail.com>
2022-09-10 21:45:11 -0500

    feat: add catch error in token

--- src/app/auth/helpers/token.interceptor.ts
+++ src/app/auth/helpers/token.interceptor.ts
@@ -1,12 +1,13 @@
-import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { AuthenticationService } from 'app/auth/service';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class TokenInterceptor implements HttpInterceptor {
-
-  constructor(private _authenticationService: AuthenticationService) { }
+  constructor(private _authenticationService: AuthenticationService, private _router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const user = this._authenticationService.currentUserValue;
@@ -16,10 +17,21 @@ export class TokenInterceptor implements HttpInterceptor {
 
     const reqClone = req.clone({
       setHeaders: {
-        Authorization: `Bearer ${user.token}`
-      }
+        Authorization: `Bearer ${user.token}`,
+      },
     });
 
-    return next.handle(reqClone);
+    return next.handle(reqClone).pipe(
+      catchError((error: any) => {
+        if (error instanceof HttpErrorResponse) {
+          if (error.status === 401 || error.status === 403) {
+            this._authenticationService.logout();
+            this._router.navigate(['/pages/authentication/login-v2']);
+          }
+        }
+
+        return throwError(error);
+      })
+    );
   }
 }
