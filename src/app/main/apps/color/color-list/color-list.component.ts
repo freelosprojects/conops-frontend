5c0f660a25556cd195e949aabd78f9384c026db9
Carlos Morales <dev.carlos.morales@gmail.com>
2022-09-10 22:03:49 -0500

    feat: color added put delete

--- src/app/main/apps/color/color-list/color-list.component.ts
+++ src/app/main/apps/color/color-list/color-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { IColor } from '@core/models/color.model';
 import { IResponseList } from '@core/models/response.model';
@@ -6,15 +6,15 @@ import { ColumnMode } from '@swimlane/ngx-datatable';
 import { Subject, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { ColorService } from '../services/color.service';
+import { ToastService } from 'app/main/components/toasts/toasts.service';
 
 @Component({
   selector: 'app-color-list',
   templateUrl: './color-list.component.html',
   styleUrls: ['./color-list.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class ColorListComponent implements OnInit, OnDestroy {
-
   public color: FormControl;
   public ColumnMode = ColumnMode;
   public selectedOption: FormControl;
@@ -25,7 +25,11 @@ export class ColorListComponent implements OnInit, OnDestroy {
   public colorSubject$: Subject<void> = new Subject();
   public subscription$: Subscription = new Subscription();
 
-  constructor(private _colorService: ColorService) {
+  message: string;
+
+  @ViewChild('toast') toast: TemplateRef<any> | null;
+
+  constructor(private _colorService: ColorService, private _toastService: ToastService) {
     this.color = new FormControl(null, Validators.required);
     this.selectedOption = new FormControl(5);
   }
@@ -42,11 +46,15 @@ export class ColorListComponent implements OnInit, OnDestroy {
     this.getColorList();
   }
 
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
+
   getColorList(): void {
     this.subscription$.add(
-      this.colorSubject$.pipe(
-        switchMap(() => this._colorService.getColorList())
-      ).subscribe(res => this.colorList = res)
+      this.colorSubject$
+        .pipe(switchMap(() => this._colorService.getColorList()))
+        .subscribe((res) => (this.colorList = res))
     );
     this.colorSubject$.next();
   }
@@ -54,9 +62,27 @@ export class ColorListComponent implements OnInit, OnDestroy {
   submitForm(): void {
     if (this.colorIsInvalid) return;
 
-    this._colorService.createColor({ color: this.color.value }).subscribe({
-      next: () => this.colorSubject$.next()
-    });
+    if (!this.itemSelected) {
+      this._colorService.createColor({ color: this.color.value }).subscribe({
+        next: (response) => {
+          this.message = response.message;
+          this.itemSelected = null;
+          this.color.reset();
+          this._toastService.showSuccess(this.toast, 'Operación exitosa');
+          this.colorSubject$.next();
+        },
+      });
+    } else {
+      this._colorService.putColor({ color: this.color.value }, this.itemSelected.idColor).subscribe({
+        next: (response) => {
+          this.message = response.message;
+          this.itemSelected = null;
+          this.color.reset();
+          this._toastService.showSuccess(this.toast, 'Operación exitosa');
+          this.colorSubject$.next();
+        },
+      });
+    }
   }
 
   edit(item: IColor) {
@@ -64,7 +90,18 @@ export class ColorListComponent implements OnInit, OnDestroy {
     this.color.setValue(item.color);
   }
 
-  ngOnDestroy(): void {
-    this.subscription$.unsubscribe();
+  delete(id: number) {
+    this._colorService.deleteColor(id).subscribe({
+      next: (response) => {
+        this.message = response.message;
+        this.itemSelected = null;
+        this._toastService.showSuccess(this.toast, 'Operación exitosa');
+        this.colorSubject$.next();
+      },
+      error: (response) => {
+        this.message = response.error.message;
+        this._toastService.showError(this.toast, 'Ocurrio un problema');
+      },
+    });
   }
 }
--- src/app/main/apps/color/services/color.service.ts
+++ src/app/main/apps/color/services/color.service.ts
@@ -8,24 +8,31 @@ import { map } from 'rxjs/operators';
 import { colorAdapter } from '../adapters/color.adapter';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColorService {
-
   private readonly _url = EndpointsRoutes.colors;
-  
+
   constructor(private _httpClient: HttpClient) {}
 
   getColorList(): Observable<IResponseList<IColor>> {
     return this._httpClient.get<IResponseList<IColorResponse>>(this._url).pipe(
-      map(response => ({
+      map((response) => ({
         count: response.count,
-        data: response.data.map(color => colorAdapter(color))
+        data: response.data.map((color) => colorAdapter(color)),
       }))
     );
   }
 
   createColor(color: IColorPost): Observable<IResponsePost> {
     return this._httpClient.post<IResponsePost>(this._url, color);
+  }
+
+  putColor(color: IColorPost, id: number): Observable<IResponsePost> {
+    return this._httpClient.put<IResponsePost>(`${this._url}/${id}`, color);
+  }
+
+  deleteColor(id: number): Observable<IResponsePost> {
+    return this._httpClient.delete<IResponsePost>(`${this._url}/${id}`);
   }
 }
